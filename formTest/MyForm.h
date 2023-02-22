358b9d1bc21684d09af21e253ffba58c09655846
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2023-02-23 03:38:31 +0800

    finish

--- formTest/MyForm.h
+++ formTest/MyForm.h
@@ -35,6 +35,18 @@ namespace formTest {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ tbFirstName;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ tbLastName;
+	private: System::Windows::Forms::Label^ lbWelcome;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Button^ btnClear;
+
+	private: System::Windows::Forms::Button^ btnOK;
+
+
+
+
 	protected:
 
 	private:
@@ -51,29 +63,161 @@ namespace formTest {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tbFirstName = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tbLastName = (gcnew System::Windows::Forms::TextBox());
+			this->lbWelcome = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->btnOK = (gcnew System::Windows::Forms::Button());
+			this->btnClear = (gcnew System::Windows::Forms::Button());
+			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Ramsella", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(16, 11);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(346, 130);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"first name";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// tbFirstName
+			// 
+			this->tbFirstName->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tbFirstName->Location = System::Drawing::Point(12, 144);
+			this->tbFirstName->Name = L"tbFirstName";
+			this->tbFirstName->Size = System::Drawing::Size(600, 22);
+			this->tbFirstName->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Ramsella", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(16, 169);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(314, 130);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"last name";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// tbLastName
+			// 
+			this->tbLastName->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tbLastName->Location = System::Drawing::Point(12, 302);
+			this->tbLastName->Name = L"tbLastName";
+			this->tbLastName->Size = System::Drawing::Size(600, 22);
+			this->tbLastName->TabIndex = 3;
+			// 
+			// lbWelcome
+			// 
+			this->lbWelcome->AutoSize = true;
+			this->lbWelcome->Font = (gcnew System::Drawing::Font(L"Ramsella", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbWelcome->Location = System::Drawing::Point(16, 327);
+			this->lbWelcome->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->lbWelcome->Name = L"lbWelcome";
+			this->lbWelcome->Size = System::Drawing::Size(163, 81);
+			this->lbWelcome->TabIndex = 4;
+			this->lbWelcome->Text = L"welcome";
+			this->lbWelcome->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel1->ColumnCount = 2;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->Controls->Add(this->btnClear, 1, 0);
+			this->tableLayoutPanel1->Controls->Add(this->btnOK, 0, 0);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(12, 502);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 1;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(600, 58);
+			this->tableLayoutPanel1->TabIndex = 5;
+			// 
+			// btnOK
+			// 
+			this->btnOK->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnOK->Font = (gcnew System::Drawing::Font(L"Ramsella", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnOK->Location = System::Drawing::Point(3, 3);
+			this->btnOK->Name = L"btnOK";
+			this->btnOK->Size = System::Drawing::Size(294, 49);
+			this->btnOK->TabIndex = 0;
+			this->btnOK->Text = L"ok";
+			this->btnOK->UseVisualStyleBackColor = true;
+			this->btnOK->Click += gcnew System::EventHandler(this, &MyForm::btnOK_Click);
+			// 
+			// btnClear
+			// 
+			this->btnClear->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnClear->Font = (gcnew System::Drawing::Font(L"Ramsella", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnClear->Location = System::Drawing::Point(303, 3);
+			this->btnClear->Name = L"btnClear";
+			this->btnClear->Size = System::Drawing::Size(294, 49);
+			this->btnClear->TabIndex = 1;
+			this->btnClear->Text = L"clear";
+			this->btnClear->UseVisualStyleBackColor = true;
+			this->btnClear->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(651, 735);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->ClientSize = System::Drawing::Size(624, 572);
+			this->Controls->Add(this->tableLayoutPanel1);
+			this->Controls->Add(this->lbWelcome);
+			this->Controls->Add(this->tbLastName);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->tbFirstName);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->MinimumSize = System::Drawing::Size(427, 619);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"welcome!";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->tableLayoutPanel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		this->lbWelcome->Text = "";
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->tbFirstName->Text = "";
+	this->tbLastName->Text = "";
+	this->lbWelcome->Text = "";
+}
+private: System::Void btnOK_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ firstName = this->tbFirstName->Text;
+	String^ lastName = this->tbLastName->Text;
+	this->lbWelcome->Text = "hello " + firstName + " " + lastName;
+}
+};
 }
